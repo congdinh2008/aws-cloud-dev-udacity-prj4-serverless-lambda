ca525915608bdc7edc5a1359b1bcd3b6286fbd91
Cong Dinh <congdinh2012@outlook.com>
2023-08-05 20:57:50 +0700

    Add helpers auth and logging

---
+++ backend/src/helpers/auth/auth.helper.ts
@@ -0,0 +1,11 @@
+import { JwtPayload, decode } from "jsonwebtoken";
+
+/**
+ * Parse a JWT token and return a user id
+ * @param jwtToken JWT token to parse
+ * @returns a user id from the JWT token
+ */
+export function parseUserId(jwtToken: string) {
+  const decodedJwt = decode(jwtToken) as JwtPayload;
+  return decodedJwt.sub;
+}
