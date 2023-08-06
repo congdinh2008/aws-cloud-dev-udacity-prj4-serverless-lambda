51fe29b644fbf681d09460ae352367fe2f378a3d
Cong Dinh <congdinh2012@outlook.com>
2023-08-07 01:09:45 +0700

    Update env auth0 api v2 link

--- client/src/components/EditTodo.jsx
+++ client/src/components/EditTodo.jsx
@@ -43,7 +43,7 @@ export function EditTodo() {
 
       setUploadState(UploadState.FetchingPresignedUrl)
       const accessToken = await getAccessTokenSilently({
-        audience: `https://test-endpoint.auth0.com/api/v2/`,
+        audience: `${process.env.AUTH0_API_V2}`,
         scope: 'write:todos'
       })
       const uploadUrl = await getUploadUrl(accessToken, todoId)
