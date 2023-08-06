33f7343d20ddd60fea48a20e9fb6181c7b81ec1a
Cong Dinh <congdinh2012@outlook.com>
2023-08-06 13:35:47 +0700

    Update api gateway with cors and add env.sh

--- backend/src/libs/api-gateway.ts
+++ backend/src/libs/api-gateway.ts
@@ -20,6 +20,10 @@ export const formatJSONResponse = (
 ) => {
   return {
     statusCode: httpStatusCode,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
+    },
     body: JSON.stringify(response),
   };
 };
