bd9d0f6616631f88acb3ba091ed271d001f8b573
Cong Dinh <congdinh2012@outlook.com>
2023-08-06 13:33:21 +0700

    Create React Component and update App Views

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useAuth0 } from '@auth0/auth0-react'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+import { Grid, Menu, Segment } from 'semantic-ui-react'
 
-function App() {
+import { EditTodo } from './components/EditTodo'
+import { LogIn } from './components/Login'
+import { NotFound } from './components/NotFound'
+import { Todos } from './components/Todos'
+
+export default function App() {
+  const generateMenu = () => {
+    return (
+      <Menu>
+        <Menu.Item as={Link} to={'/'}>
+          Home
+        </Menu.Item>
+
+        <Menu.Menu position="right">{logInLogOutButton()}</Menu.Menu>
+      </Menu>
+    )
+  }
+
+  const logInLogOutButton = () => {
+    if (isAuthenticated) {
+      return (
+        <Menu.Item name="logout" onClick={() => logout()}>
+          Log Out
+        </Menu.Item>
+      )
+    } else {
+      return (
+        <Menu.Item name="login" onClick={() => loginWithRedirect()}>
+          Log In
+        </Menu.Item>
+      )
+    }
+  }
+
+  const { isAuthenticated, loginWithRedirect, logout } = useAuth0()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Segment style={{ padding: '8em 0em' }} vertical>
+        <Grid container stackable verticalAlign="middle">
+          <Grid.Row>
+            <Grid.Column width={16}>
+              <BrowserRouter>
+                {generateMenu()}
+
+                {generateCurrentPage(isAuthenticated)}
+              </BrowserRouter>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
     </div>
-  );
+  )
 }
 
-export default App;
+function generateCurrentPage(isAuthenticated: boolean) {
+  if (!isAuthenticated) {
+    return <LogIn />
+  }
+
+  return (
+    <Routes>
+      <Route path="/" element={<Todos />} />
+
+      <Route path="/todos/:todoId/edit" element={<EditTodo />} />
+
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  )
+}
